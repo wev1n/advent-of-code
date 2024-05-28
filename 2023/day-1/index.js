ab291f558b85aae7fb158a1b2928bb019dcc578b
wevin <kevin.warme@gmail.com>
2024-05-28 18:53:59 +0200

    day-1 part two completed

--- 2023/day-1/index.js
+++ 2023/day-1/index.js
@@ -1,20 +1,40 @@
-import fs from "fs";
+import { readFileSync } from "fs";
 
-function partOne(file) {
-  const lines = fs
-    .readFileSync(file, "utf-8")
-    .trim()
-    .replace(/\r/g, "")
-    .split("\n");
+let inputs = readFileSync("./input.txt", "utf-8")
+  .split("\r\n")
+  .filter((x) => !!x);
 
-  const values = lines.map((line) => {
-    let first = line.split("").find((v) => !Number.isNaN(Number(v)));
-    let last = line.split("").findLast((v) => !Number.isNaN(Number(v)));
-    return Number(first + last);
-  });
+let sum = 0;
+const numbers = "123456789";
 
-  return values.reduce((s, v) => s + v);
-}
+const numbersAsStrings = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
 
-// console.log(partOne("./example.txt"));
-console.log(partOne("./input.txt"));
+inputs.forEach((word) => {
+  let numbersArray = [];
+
+  for (let i = 0; i < word.length; i++) {
+    if (numbers.includes(word[i])) {
+      numbersArray.push(word[i]);
+    }
+
+    for (let j = 3; j <= 5; j++) {
+      if (numbersAsStrings[word.substring(i, i + j)] !== undefined) {
+        numbersArray.push(numbersAsStrings[word.substring(i, i + j)]);
+      }
+    }
+  }
+
+  sum += parseInt(numbersArray[0] + "" + numbersArray[numbersArray.length - 1]);
+});
+
+console.log(sum);
