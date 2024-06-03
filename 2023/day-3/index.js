be58f0216e3ffe987f9fd3ed74b2d94f4027afe3
wevin <kevin.warme@gmail.com>
2024-06-03 21:45:08 +0200

    day-3, p2 completed

--- 2023/day-3/index.js
+++ 2023/day-3/index.js
@@ -1,13 +1,15 @@
-import fs from "fs"
+import fs from "fs";
 
+// -----------Helpers-------------
 function hasSymbol(str) {
-  if (str?.length && str.split("").find(x => isNaN(x) && x !== '.')) {
+  if (str?.length && str.split("").find((x) => isNaN(x) && x !== ".")) {
     return true;
   } else {
     return false;
   }
 }
 
+// ------------P1-----------------
 function p1(file) {
   const input = fs
     .readFileSync(file, "utf-8")
@@ -34,18 +36,86 @@ function p1(file) {
         }
       }
 
-      const top = i === 0 ? "" : input[i-1].substring(j - num.length - 1, j + 1)
-      const bottom = i === rows - 1 ? "" : input[i+1].substring(j - num.length - 1, j + 1)
-      const left = input[i][j - num.length - 1] || ""
-      const right = input[i][j] || ""
+      const top =
+        i === 0 ? "" : input[i - 1].substring(j - num.length - 1, j + 1);
+      const bottom =
+        i === rows - 1 ? "" : input[i + 1].substring(j - num.length - 1, j + 1);
+      const left = input[i][j - num.length - 1] || "";
+      const right = input[i][j] || "";
 
-      if(hasSymbol(top) || hasSymbol(bottom) || hasSymbol(right) || hasSymbol(left)){
-        founds.push(Number(num))
+      if (
+        hasSymbol(top) ||
+        hasSymbol(bottom) ||
+        hasSymbol(right) ||
+        hasSymbol(left)
+      ) {
+        founds.push(Number(num));
       }
     }
   }
 
-  return founds.reduce((a, c) => a + c, 0)
+  return founds.reduce((a, c) => a + c, 0);
 }
 
 console.log(`P1: ${p1("./input.txt")}`);
+
+// ------------P2-----------------
+function p2(file) {
+  const input = fs
+    .readFileSync(file, "utf-8")
+    .trim()
+    .replace(/\r/g, "")
+    .split("\n");
+
+  let rows = input.length;
+  let cols = input[0].length;
+
+  const gearsDic = {};
+
+  const findGears = (str, num, i, j) => {
+    j = j === -1 ? 0 : j;
+    for (let k = 0; k < str.length; k++) {
+      const ch = str.charAt(k);
+      if (ch === "*") {
+        const ind = `${i}-${j + k}`;
+        gearsDic[ind] = gearsDic[ind]
+          ? [...gearsDic[ind], parseInt(num)]
+          : [parseInt(num)];
+      }
+    }
+  };
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      const n = "" + input[i][j];
+      if (isNaN(n)) continue;
+
+      let num = n;
+      while (++j < cols) {
+        if (Number.isInteger(parseInt(input[i][j]))) num += input[i][j];
+        else break;
+      }
+
+      const top =
+        i === 0 ? "" : input[i - 1].substring(j - num.length - 1, j + 1);
+      const btm =
+        i === rows - 1 ? "" : input[i + 1].substring(j - num.length - 1, j + 1);
+      const lft = input[i][j - num.length - 1] || "";
+      const rgt = input[i][j] || "";
+
+      findGears(top, num, i - 1, j - num.length - 1);
+      findGears(btm, num, i + 1, j - num.length - 1);
+      findGears(lft, num, i, j - num.length - 1);
+      findGears(rgt, num, i, j);
+    }
+  }
+
+  const v = Object.values(gearsDic)
+    .filter((x) => x.length === 2)
+    .map((x) => x[0] * x[1])
+    .reduce((a, x) => a + x, 0);
+
+  return v;
+}
+
+console.log(`P2: ${p2("./input.txt")}`);
