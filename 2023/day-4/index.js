d30ef1fb22e2433078f91f97deddd3940c0dc14f
wevin <kevin.warme@gmail.com>
2024-06-06 22:13:46 +0200

    2023 day-4 p2

--- 2023/day-4/index.js
+++ 2023/day-4/index.js
@@ -1,7 +1,8 @@
 import * as fs from "fs";
 
-// ------------P1-----------------
 const lines = fs.readFileSync("input.txt", "utf-8").split("\n");
+
+// ------------P1-----------------
 let result = 0;
 
 lines.forEach((line) => {
@@ -24,4 +25,36 @@ function countCardPoints(line) {
     output += 2 ** (commonItems.length - 1);
   }
   return output;
+}
+
+// ------------P2-----------------
+let cardInstances = [];
+let count = 0;
+
+lines.forEach((line, i) => {
+  count += countCardInstances(line, i);
+});
+
+console.log(count);
+
+function countCardInstances(line, cardNumber) {
+  const lists = line.split(":")[1].trim().split("|");
+
+  const [winningNumbers, myNumbers] = lists.map(
+    (item) => item.match(/\d+/g)?.map(Number) || []
+  );
+  const commonItems = myNumbers.filter((item) => winningNumbers.includes(item));
+
+  cardInstances[cardNumber] = (cardInstances[cardNumber] ?? 0) + 1;
+
+  for (let j = 0; j < cardInstances[cardNumber]; j++) {
+    if (commonItems.length === 0) {
+      continue;
+    }
+    for (let i = 1; i <= commonItems.length; i++) {
+      cardInstances[cardNumber + i] = (cardInstances[cardNumber + i] ?? 0) + 1;
+    }
+  }
+
+  return cardInstances[cardNumber];
 }
