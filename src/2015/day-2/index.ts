4c01c7e9fd475bd37778f9c5c4bfb9f3d60157aa
wevin <kevin.warme@gmail.com>
2024-10-17 20:53:15 +0200

    day-2 2015 p1 completed

--- src/2015/day-2/index.ts
+++ src/2015/day-2/index.ts
@@ -1,1 +1,37 @@
-console.log("Hello mum!");
+import * as fs from "fs";
+
+function readInputFile(file: string): string[] {
+  return fs.readFileSync(file, "utf-8").trim().replace(/\r/g, "").split("\n");
+}
+
+// 2*l*w + 2*w*h + 2*h*l
+function calculateWrappingPaper(l: number, w: number, h: number): number {
+  const s1 = l * w;
+  const s2 = w * h;
+  const s3 = h * l;
+
+  const surfaceArea = 2 * s1 + 2 * s2 + 2 * s3;
+  const smallestSide = Math.min(s1, s2, s3);
+
+  return surfaceArea + smallestSide;
+}
+
+// ------------P1-----------------
+function p1(file: string): number {
+  const lines = readInputFile(file);
+
+  const result: number[] = lines.map((line) => {
+    const dimensions = line.trim().split("x").map(Number);
+
+    if (dimensions.length === 3) {
+      const [l, w, h] = dimensions;
+      return calculateWrappingPaper(l, w, h);
+    } else {
+      return 0;
+    }
+  });
+
+  return result.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(`P1: ${p1("./src/2015/day-2/input.txt")}`);
