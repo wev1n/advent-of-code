89c2b1d22ce31c42e121b7b482e7ac6a94a82a79
wevin <kevin.warme@gmail.com>
2024-10-18 20:28:28 +0200

    day-2 2015 p2

--- src/2015/day-2/index.ts
+++ src/2015/day-2/index.ts
@@ -16,6 +16,16 @@ function calculateWrappingPaper(l: number, w: number, h: number): number {
   return surfaceArea + smallestSide;
 }
 
+// 2x3x4, 2+2+3+3 = 10 + 2*3*4 = 24
+function calculateRibbon(l: number, w: number, h: number): number {
+  const perimeters = [2 * (l + w), 2 * (w + h), 2 * (h + l)];
+
+  const ribbonLength = Math.min(...perimeters);
+  const ribbonBow = l * w * h;
+
+  return ribbonLength + ribbonBow;
+}
+
 // ------------P1-----------------
 function p1(file: string): number {
   const lines = readInputFile(file);
@@ -35,3 +45,23 @@ function p1(file: string): number {
 }
 
 console.log(`P1: ${p1("./src/2015/day-2/input.txt")}`);
+
+// ------------P2-----------------
+function p2(file: string): number {
+  const lines = readInputFile(file);
+
+  const result: number[] = lines.map((line) => {
+    const dimensions = line.trim().split("x").map(Number);
+
+    if (dimensions.length === 3) {
+      const [l, w, h] = dimensions;
+      return calculateRibbon(l, w, h);
+    } else {
+      return 0;
+    }
+  });
+
+  return result.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(`P2: ${p2("./src/2015/day-2/input.txt")}`);
