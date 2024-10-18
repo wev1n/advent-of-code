cb84a411d00ae43f6e5ef3959844fef2aa130191
wevin <kevin.warme@gmail.com>
2024-10-18 21:04:39 +0200

    day-3 2015 p2

--- src/2015/day-3/index.ts
+++ src/2015/day-3/index.ts
@@ -33,3 +33,50 @@ function p1(file: string): number {
 }
 
 console.log(`P1: ${p1("./src/2015/day-3/input.txt")}`);
+
+// ------------P2-----------------
+function p2(file: string): number {
+  const directions = readInputFile(file);
+
+  let santaX = 0,
+    santaY = 0;
+  let robotX = 0,
+    robotY = 0;
+
+  const visited = new Set<String>();
+
+  visited.add(`${santaX},${santaY}`);
+  visited.add(`${robotX},${robotY}`);
+
+  for (let i = 0; i < directions.length; i++) {
+    const direction = directions[i];
+
+    if (i % 2 === 0) {
+      if (direction === "^") {
+        santaY += 1;
+      } else if (direction === "v") {
+        santaY -= 1;
+      } else if (direction === ">") {
+        santaX += 1;
+      } else if (direction === "<") {
+        santaX -= 1;
+      }
+      visited.add(`${santaX},${santaY}`);
+    } else {
+      if (direction === "^") {
+        robotY += 1;
+      } else if (direction === "v") {
+        robotY -= 1;
+      } else if (direction === ">") {
+        robotX += 1;
+      } else if (direction === "<") {
+        robotX -= 1;
+      }
+      visited.add(`${robotX},${robotY}`);
+    }
+  }
+
+  return visited.size;
+}
+
+console.log(`P2: ${p2("./src/2015/day-3/input.txt")}`);
