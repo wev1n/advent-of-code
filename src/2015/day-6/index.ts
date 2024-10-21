936f4cd51d10e15ed23661eed0d7740b274e7d8d
wevin <kevin.warme@gmail.com>
2024-10-21 19:54:03 +0200

    day-6 2015 p2

--- src/2015/day-6/index.ts
+++ src/2015/day-6/index.ts
@@ -5,8 +5,12 @@ function readInputFile(file: string): string[] {
 }
 
 // ------------Helpers------------
-function createGrid(): boolean[][] {
+function createBooleanGrid(): boolean[][] {
   return Array.from({ length: 1000 }, () => Array(1000).fill(false));
+}
+
+function createNumberGrid(): number[][] {
+  return Array.from({ length: 1000 }, () => Array(1000).fill(0));
 }
 
 function parseInstruction(
@@ -47,7 +51,6 @@ function parseInstruction(
 
 function countLitLights(grid: boolean[][]): number {
   let count = 0;
-
   for (let x = 0; x < 1000; x++) {
     for (let y = 0; y < 1000; y++) {
       if (grid[x][y]) {
@@ -59,16 +62,26 @@ function countLitLights(grid: boolean[][]): number {
   return count;
 }
 
+function totalBrightness(grid: number[][]): number {
+  let total = 0;
+  for (let x = 0; x < 1000; x++) {
+    for (let y = 0; y < 1000; y++) {
+      total += grid[x][y];
+    }
+  }
+
+  return total;
+}
+
 // ------------P1-----------------
 function p1(file: string): number {
   const instructions = readInputFile(file);
-  const grid = createGrid();
+  const grid = createBooleanGrid();
 
   instructions.forEach((instruction) => {
     const [action, start, end] = parseInstruction(instruction);
     const [x1, y1]: [number, number] = start;
     const [x2, y2]: [number, number] = end;
-
     for (let x = Number(x1); x <= Number(x2); x++) {
       for (let y = Number(y1); y <= Number(y2); y++) {
         if (action === "turn on") {
@@ -86,3 +99,31 @@ function p1(file: string): number {
 }
 
 console.log(`P1: ${p1("./src/2015/day-6/input.txt")}`);
+
+// ------------P2-----------------
+function p2(file: string): number {
+  const instructions = readInputFile(file);
+  const grid = createNumberGrid();
+
+  instructions.forEach((instruction) => {
+    const [action, start, end] = parseInstruction(instruction);
+    const [x1, y1]: [number, number] = start;
+    const [x2, y2]: [number, number] = end;
+
+    for (let x = Number(x1); x <= Number(x2); x++) {
+      for (let y = Number(y1); y <= Number(y2); y++) {
+        if (action === "turn on") {
+          grid[x][y] += 1;
+        } else if (action === "turn off") {
+          grid[x][y] = Math.max(0, grid[x][y] - 1);
+        } else if (action === "toggle") {
+          grid[x][y] += 2;
+        }
+      }
+    }
+  });
+
+  return totalBrightness(grid);
+}
+
+console.log(`P2: ${p2("./src/2015/day-6/input.txt")}`);
