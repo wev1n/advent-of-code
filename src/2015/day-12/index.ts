d0f3bb6598954e1cbd814cef54e273f45c92bde9
wevin <kevin.warme@gmail.com>
2024-11-12 19:56:03 +0100

    day-12 2015 p2

--- src/2015/day-12/index.ts
+++ src/2015/day-12/index.ts
@@ -43,3 +43,38 @@ function p1(data: any): number {
 
 const jsonData = readInputFile("./src/2015/day-12/input.txt");
 console.log(`P1: ${p1(jsonData)}`);
+
+// ------------P2-----------------
+function p2(data: any): number {
+  if (data === null || data === undefined) {
+    return 0;
+  }
+
+  let total = 0;
+
+  if (typeof data === "number") {
+    return data;
+  }
+
+  if (Array.isArray(data)) {
+    for (const item of data) {
+      total += p2(item);
+    }
+  } else if (typeof data === "object") {
+    for (const key in data) {
+      if (data.hasOwnProperty(key) && data[key] === "red") {
+        return 0;
+      }
+    }
+
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        total += p2(data[key]);
+      }
+    }
+  }
+
+  return total;
+}
+
+console.log(`P2: ${p2(jsonData)}`);
