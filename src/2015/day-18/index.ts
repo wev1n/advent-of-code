66c4c5dcc99f52ef0ba4e0933698de53b2a6fc12
wevin <kevin.warme@gmail.com>
2025-01-05 19:58:34 +0100

    day+18 2015 p2

--- src/2015/day-18/index.ts
+++ src/2015/day-18/index.ts
@@ -32,11 +32,18 @@ function countOnNeighbors(grid: string[][], x: number, y: number): number {
   return count;
 }
 
-function getNextState(grid: string[][]): string[][] {
+function getNextState(
+  grid: string[][],
+  stuckCorners: boolean = false
+): string[][] {
   const newGrid = grid.map((row) => [...row]);
 
   for (let x = 0; x < grid.length; x++) {
     for (let y = 0; y < grid[0].length; y++) {
+      if (stuckCorners && isCorner(x, y, grid.length, grid[0].length)) {
+        continue;
+      }
+
       const onNeighbors = countOnNeighbors(grid, x, y);
 
       if (grid[x][y] === "#") {
@@ -47,11 +54,33 @@ function getNextState(grid: string[][]): string[][] {
     }
   }
 
+  if (stuckCorners) {
+    turnOnCorners(newGrid);
+  }
+
   return newGrid;
 }
 
 function countOnLights(grid: string[][]): number {
   return grid.flat().filter((cell) => cell === "#").length;
+}
+
+function isCorner(x: number, y: number, rows: number, cols: number): boolean {
+  return (
+    (x === 0 && y === 0) ||
+    (x === 0 && y === cols - 1) ||
+    (x === rows - 1 && y === 0) ||
+    (x === rows - 1 && y === cols - 1)
+  );
+}
+
+function turnOnCorners(grid: string[][]): void {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  grid[0][0] = "#";
+  grid[0][cols - 1] = "#";
+  grid[rows - 1][0] = "#";
+  grid[rows - 1][cols - 1] = "#";
 }
 
 // ------------P1-----------------
@@ -66,3 +95,18 @@ function p1(file: string): number {
 }
 
 console.log(`P1: ${p1("./src/2015/day-18/input.txt")}`);
+
+// ------------P2-----------------
+function p2(file: string): number {
+  let grid = readInputFile(file).map((line) => line.split(""));
+
+  turnOnCorners(grid);
+
+  for (let step = 0; step < 100; step++) {
+    grid = getNextState(grid, true);
+  }
+
+  return countOnLights(grid);
+}
+
+console.log(`P2: ${p2("./src/2015/day-18/input.txt")}`);
